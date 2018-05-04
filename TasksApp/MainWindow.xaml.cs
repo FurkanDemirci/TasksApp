884993b4126d01d78b9cb68f23b3b62bc1d85041
Furkan Demirci <furkan.demirci@live.nl>
2018-05-04 17:17:43 +0200

    New project with emirhan.

--- TasksApp/MainWindow.xaml.cs
+++ TasksApp/MainWindow.xaml.cs
@@ -12,17 +12,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace TasksApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            RightFlyout.IsOpen = !RightFlyout.IsOpen;
         }
     }
 }
